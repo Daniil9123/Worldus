b6c8d04660bac8d82b6e1df49755b37e90fe5dc7
Daniil9123 <daniil.yanchuk@ivkhk.ee>
2026-04-27 20:32:37 +0300

    Добавление ERROR 404

---
+++ error404.php
@@ -0,0 +1,18 @@
+<?php include "includes/header.php"; ?>
+
+<div class="container">
+    <div class="card error-card">
+        <h1>ERROR 404</h1>
+        <!-- <h2>Страница не найдена</h2>
+
+        <p>
+            
+        </p> -->
+
+        <div class="buttons">
+            <a href="index.php" class="btn blue">На главную</a>
+        </div>
+    </div>
+</div>
+
+<?php include "includes/footer.php"; ?>
