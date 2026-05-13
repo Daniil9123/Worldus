891baffb261b3e2c960b79db91fa370693ee8b0c
Daniil Yanchuk <daniil.yanchuk@ivkhk.ee>
2026-05-13 12:17:00 +0300

    Полная доработка ERROR404.

--- error404.php
+++ error404.php
@@ -1,38 +1,26 @@
-<<<<<<< HEAD
 <?php include "includes/header.php"; ?>
 
 <div class="container">
+
     <div class="card error-card">
+
         <h1>ERROR 404</h1>
-        <!-- <h2>Страница не найдена</h2>
+
+        <h2>Страница не найдена</h2>
 
         <p>
-            
-        </p> -->
+            Возможно страница была удалена
+            или адрес введён неправильно.
+        </p>
 
         <div class="buttons">
-            <a href="index.php" class="btn blue">На главную</a>
+            <a href="index.php" class="btn blue">
+                На главную
+            </a>
         </div>
+
     </div>
+
 </div>
 
-=======
-<?php include "includes/header.php"; ?>
-
-<div class="container">
-    <div class="card error-card">
-        <h1>ERROR 404</h1>
-        <!-- <h2>Страница не найдена</h2>
-
-        <p>
-            
-        </p> -->
-
-        <div class="buttons">
-            <a href="index.php" class="btn blue">На главную</a>
-        </div>
-    </div>
-</div>
-
->>>>>>> b6c8d04660bac8d82b6e1df49755b37e90fe5dc7
 <?php include "includes/footer.php"; ?>
